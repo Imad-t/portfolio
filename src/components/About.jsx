7633f6102c28b3dc729b977752805c6a82cf9dc3
Imad-t <imadeddineterraf@gmail.com>
2023-06-15 02:30:44 +0100

    home page and about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,33 @@
 import styles from "../styles/About.module.scss";
+import image from "../assets/images/about.png";
 
 const About = () => {
-    return ( 
-        <div className={styles.about}>
-            About
-        </div>
-     );
-}
- 
+  return (
+    <div className={styles.about}>
+      <img src={image} alt="" />
+      <div className={styles.section}>
+        <h3>About me</h3>
+        <h2>A dedicated Front-end Developer based in Algiers, Algeria.</h2>
+        <p>
+          As a junior frontend developer with experience in React, I have a
+          solid foundation in building dynamic and responsive web applications.
+        </p>
+        <p>
+          With a focus on problem-solving, I strive to deliver innovative
+          solutions that enhance user experiences. Currently in my final year of
+          computer science studies, I am equipped with a deep understanding of
+          programming principles and best practices.
+        </p>
+        <p>
+             I am highly motivated and
+          constantly seeking opportunities to expand my skill set and stay
+          up-to-date with the latest industry trends. Collaboration and teamwork
+          are key aspects of my approach, and I thrive in a dynamic and
+          innovative environment.
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,52 @@
 import styles from "../styles/Home.module.scss";
+import Myimage from "../assets/images/20221221141125_IMG_0137.jpg";
+import linkedin from "../assets/images/linkedin.png";
+import github from "../assets/images/github.png";
+import html from "../assets/images/html.png";
+import css from "../assets/images/css.png";
+import sass from "../assets/images/sass.png";
+import js from "../assets/images/js.png";
+import react from "../assets/images/react.png";
+import bootstrap from "../assets/images/bootstrap.png";
+import python from "../assets/images/python.png";
+import chakra from "../assets/images/chakra.png";
+import git from "../assets/images/git.png";
+import sql from "../assets/images/sql.png";
+
 const Home = () => {
-    return ( 
-        <div className={styles.home}>
-            Home
+  return (
+    <div className={styles.home}>
+      <div className={styles.info}>
+        <div>
+          <h1>Front-End React Developer</h1>
+          <p>
+            Hi, I'm Imad-Eddine Terraf, a passionate front-end React developer based in
+            Algiers, Algeria.
+          </p>
+          <a href="https://www.linkedin.com/in/imad-eddine-terraf-745a0b201/">
+            <img className={styles.contact} src={linkedin} alt="Linkedin " />
+          </a>
+          <a href="https://github.com/Imad-t">
+            <img className={styles.contact} src={github} alt="Github " />
+          </a>
         </div>
-     );
-}
- 
+        <div className={styles.stack}>
+           <span>Tech Stack</span>
+          <img className={styles.icon} src={html} alt="html " />
+          <img className={styles.icon} src={css} alt="css " />
+          <img className={styles.icon} src={sass} alt="sass " />
+          <img className={styles.icon} src={js} alt="js " />
+          <img className={styles.icon} src={react} alt="react " />
+          <img className={styles.icon} src={chakra} alt="chakra " />
+          <img className={styles.icon} src={git} alt="git " />
+          <img className={styles.icon} src={bootstrap} alt="bootstrap " />
+          <img className={styles.icon} src={python} alt="python " />
+          <img className={styles.icon} src={sql} alt="sql " />
+        </div>
+      </div>
+      <img className={styles.myImage} src={Myimage} alt="Imad's picture" />
+    </div>
+  );
+};
+
 export default Home;
