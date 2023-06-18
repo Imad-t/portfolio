15db592eb28cfe02a80489393ec11834ee41c4ed
Imad-t <imadeddineterraf@gmail.com>
2023-06-18 04:28:11 +0100

    finished all pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,69 @@
 import styles from "../styles/Contact.module.scss";
-const Home = () => {
-    return ( 
-        <div className={styles.contact}>
-            Contact
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import EmailIcon from "@mui/icons-material/Email";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import DiscordIcon from "../assets/images/discord.png";
+import { useState } from "react";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Contact = () => {
+
+    const [copySuccess, setCopySuccess] = useState('');
+
+    const handleCopy = (text) => {
+        navigator.clipboard.writeText(text)
+          .then(() => {
+            toast.success(`Copied: ${text}`);
+          })
+          .catch((error) => {
+            console.error('Failed to copy text:', error);
+          });
+      };
+
+  return (
+    <div className={styles.contact}>
+      <p className={styles.contactMe}>
+        If you are intrested in working with me or if you have any questions, do
+        not hesitate to contact me !{" "}
+      </p>
+
+      <div className={styles.contactInfo}>
+        <div className={styles.inline}>
+          <LocationCityIcon className={styles.icon} />
+          <div>
+            <p>Location</p>
+            <p>Bab Ezzouar, Algiers, Algeria</p>
+          </div>
         </div>
-     );
-}
- 
-export default Home;
+
+        <div className={styles.inline}>
+          <EmailIcon className={styles.icon} onClick={() => handleCopy('imadeddineterraf@gmail.com')}/>
+          <div>
+            <p>Email</p>
+            <p>imadeddineterraf@gmail.com</p>
+          </div>
+        </div>
+        <div className={styles.sm}>
+          <a href="https://www.linkedin.com/in/imad-eddine-terraf-745a0b201/">
+            <LinkedInIcon className={styles.icon} />
+          </a>
+          <a href="https://www.facebook.com/imad.terraf/">
+            <FacebookIcon className={styles.icon} />
+          </a>
+          <a href="https://www.instagram.com/i_mad.t/">
+            <InstagramIcon className={styles.icon} />
+          </a>
+          <a href="#">
+            <img src={DiscordIcon} alt="discord logo" className={styles.icon} />
+          </a>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,89 @@
 import styles from "../styles/Projects.module.scss";
+import chatPage from "../assets/images/chatPage.png";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
 const Home = () => {
-    return ( 
-        <div className={styles.projects}>
-            Projects
+  return (
+    <div className={styles.projects}>
+
+      <div className={styles.project}>
+        <img src={chatPage} alt="chatPage" />
+
+        <div className={styles.info}>
+          <h2>Chat Page</h2>
+          <p>
+            This is a simple chat page, suitable for any sort of website that
+            supports account registration.{" "}
+          </p>
+
+          <div className={styles.tech}>
+            <span>React</span> <span>Scss</span>
+          </div>
+
+          <div className={styles.links}>
+            <a href="https://github.com/Imad-t/chatPage">
+              <GitHubIcon /> Code
+            </a>
+            <a href="https://chat-page-zeta.vercel.app/">
+              <LaunchIcon /> Live demo
+            </a>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+
+      <div className={styles.project}>
+
+        <div className={styles.info}>
+          <h2>Chat Page</h2>
+          <p>
+            This is a simple chat page, suitable for any sort of website that
+            supports account registration.{" "}
+          </p>
+
+          <div className={styles.tech}>
+            <span>React</span> <span>Scss</span>
+          </div>
+
+          <div className={styles.links}>
+            <a href="https://github.com/Imad-t/chatPage">
+              <GitHubIcon /> Code
+            </a>
+            <a href="https://chat-page-zeta.vercel.app/">
+              <LaunchIcon /> Live demo
+            </a>
+          </div>
+        </div>
+
+        <img src={chatPage} alt="chatPage" />
+
+      </div>
+
+      <div className={styles.project}>
+        <img src={chatPage} alt="chatPage" />
+
+        <div className={styles.info}>
+          <h2>Chat Page</h2>
+          <p>
+            This is a simple chat page, suitable for any sort of website that
+            supports account registration.{" "}
+          </p>
+
+          <div className={styles.tech}>
+            <span>React</span> <span>Scss</span>
+          </div>
+
+          <div className={styles.links}>
+            <a href="https://github.com/Imad-t/chatPage">
+              <GitHubIcon /> Code
+            </a>
+            <a href="https://chat-page-zeta.vercel.app/">
+              <LaunchIcon /> Live demo
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
