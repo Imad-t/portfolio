0078c8dedb96c1f37c90b7d85e66307390a5c8b7
Imad-t <imadeddineterraf@gmail.com>
2024-08-11 21:15:53 +0100

    mapped through projects + added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,161 +1,44 @@
 import styles from "../styles/Projects.module.scss";
-import chatPage from "../assets/images/chatPage.png";
-import musicPlayer from "../assets/images/musicPlayer.png";
-import multiStep from "../assets/images/multiStep1.png";
-import skyWay from "../assets/images/skyWay.png";
-import alarado from "../assets/images/alarado.png";
-import translator from "../assets/images/translator.png";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LaunchIcon from "@mui/icons-material/Launch";
+import projects from "../data";
+import PropTypes from "prop-types";
 
-const Home = ({darkMode}) => {
+const Home = ({ darkMode }) => {
   return (
-    <div className={`${styles.projects} ${darkMode ? styles["dark-mode"] : ""}`}>
-      <div className={styles.project}>
-                <div className={styles.info}>
-          <h2>Translator App</h2>
-          <p>
-            A single page translator app that utilizes MyMemory API and provides basic text to speech.
-          </p>
+    <div
+      className={`${styles.projects} ${darkMode ? styles["dark-mode"] : ""}`}
+    >
+      {projects.map((project, index) => (
+        <div key={index} className={styles.project}>
+          <div className={styles.info}>
+            <h2>{project.title}</h2>
+            <p>
+              {project.description}
+            </p>
 
-          <div className={styles.tech}>
-            <span>React</span> <span>TailwindCss</span>
-          </div>
+            <div className={styles.tech}>
+              {project.tech.map((tech, index) => (
+                <span key={index}>{tech}</span>
+              ))}
+            </div>
 
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/translator-app">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://translated-io.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
+            <div className={styles.links}>
+              <a href={project.code}>
+                <GitHubIcon /> <span>Code</span>
+              </a>
+              <a href={project.demo}>
+                <LaunchIcon /> <span>Live Demo</span>
+              </a>
+            </div>
           </div>
+          <img src={project.img} alt="translator website image" />
         </div>
-        <img src={translator} alt="translator website image" />
-
-      </div>
-      <div className={styles.project}>
-                <div className={styles.info}>
-          <h2>Responsive Hero Page</h2>
-          <p>
-            A responsive Hero page with Dark mode toggle
-          </p>
-
-          <div className={styles.tech}>
-            <span>Javascript</span> <span>Css</span>
-          </div>
-
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/Darkmode-Homepage">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://alarado-ivory.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
-          </div>
-        </div>
-        <img src={alarado} alt="chatPage" />
-
-      </div>
-
-      <div className={styles.project}>
-                <div className={styles.info}>
-          <h2>Multistep from</h2>
-          <p>
-            A multistep register form with simple validation using vanilla Javascript
-          </p>
-
-          <div className={styles.tech}>
-            <span>Javascript</span> <span>CSS</span>
-          </div>
-
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/multi-step-formr">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://multi-step-form-wine-alpha.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
-          </div>
-        </div>
-        <img src={multiStep} alt="musicPlayer" />
-      </div>
-
-      <div className={styles.project}>
-                <div className={styles.info}>
-          <h2>Music Player</h2>
-          <p>
-            An online music player with custom playback controls.
-          </p>
-
-          <div className={styles.tech}>
-            <span>Javascript</span> <span>Tailwindcss</span>
-          </div>
-
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/music-player">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://music-player-imadt.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
-          </div>
-        </div>
-        <img src={musicPlayer} alt="musicPlayer" />
-
-      </div>
-
-      <div className={styles.project}>
-        <div className={styles.info}>
-          <h2>Flight reservation website</h2>
-          <p>
-            A simple attemt at creating the UI of a flight reservation website.
-          </p>
-
-          <div className={styles.tech}>
-            <span>React</span> <span>Typescript</span> <span>Tailwind CSS</span>
-          </div>
-
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/skyWay">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://sky-way.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
-          </div>
-        </div>
-
-        <img src={skyWay} alt="skyWay" />
-      </div>
-
-      <div className={styles.project}>
-                <div className={styles.info}>
-          <h2>Chat Page</h2>
-          <p>
-            This is a simple chat page, suitable for any sort of website that
-            supports account registration.{" "}
-          </p>
-
-          <div className={styles.tech}>
-            <span>React</span> <span>Scss</span>
-          </div>
-
-          <div className={styles.links}>
-            <a href="https://github.com/Imad-t/chatPage">
-              <GitHubIcon /> <span>Code</span>
-            </a>
-            <a href="https://chat-page-zeta.vercel.app/">
-              <LaunchIcon /> <span>Live Demo</span>
-            </a>
-          </div>
-        </div>
-        <img src={chatPage} alt="chatPage" />
-
-      </div>
-      
+      ))}
     </div>
   );
 };
-
+Home.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+};
 export default Home;
---
+++ src/data.js
@@ -0,0 +1,98 @@
+import chatPage from "./assets/images/chatPage.png";
+import musicPlayer from "./assets/images/musicPlayer.png";
+import multiStep from "./assets/images/multiStep1.png";
+import skyWay from "./assets/images/skyWay.png";
+import alarado from "./assets/images/alarado.png";
+import translator from "./assets/images/translator.png";
+import quiz from "./assets/images/quiz.png";
+
+const projects = [
+  {
+    title: "Online Gallery",
+    description:
+      "A Gallery website that allows users to sign up and upload their own images using the T3 stack.",
+    tech: ["NextJS", "PostgreSQL", "Prisma","Clerk", "TailwindCss"],
+    links: {
+      code: "https://github.com/Imad-t/translator-app",
+      demo: "https://translated-io.vercel.app/",
+    },
+    img: translator,
+  },
+  {
+    title: "Translator App",
+    description:
+      "A single page translator app that utilizes MyMemory API and provides basic text to speech.",
+    tech: ["React", "TailwindCss"],
+    links: {
+      code: "https://github.com/Imad-t/translator-app",
+      demo: "https://translated-io.vercel.app/",
+    },
+    img: translator,
+  },
+  {
+    title: "Country Quiz",
+    description:
+      "A quiz game that tests users knowledge of countries and their flags utilizing the REST countries API.",
+    tech: ["React", "TailwindCss"],
+    links: {
+      code: "https://github.com/Imad-t/countryQuiz",
+      demo: "https://country-quiz-puce.vercel.app/",
+    },
+    img: quiz,
+  },
+  {
+    title: "Responsive Hero Page",
+    description: "A responsive Hero page with Dark mode toggle.",
+    tech: ["JavaScript", "CSS"],
+    links: {
+      code: "https://github.com/Imad-t/Darkmode-Homepage",
+      demo: "https://alarado-ivory.vercel.app/",
+    },
+    img: alarado,
+  },
+  {
+    title: "Multistep from",
+    description:
+      "A multistep register form with simple validation using vanilla Javascript.",
+    tech: ["JavaScript", "CSS"],
+    links: {
+      code: "https://github.com/Imad-t/multi-step-form",
+      demo: "https://multi-step-form-wine-alpha.vercel.app/",
+    },
+    img: multiStep,
+  },
+  {
+    title: "Music Player",
+    description: "An online music player with custom playback controls.",
+    tech: ["JavaScript", "TailwindCss"],
+    links: {
+      code: "https://github.com/Imad-t/music-player",
+      demo: "https://music-player-imadt.vercel.app/",
+    },
+    img: musicPlayer,
+  },
+  {
+    title: "Flight reservation website",
+    description:
+      "A simple attempt at creating the UI of a flight reservation website.",
+    tech: ["React", "TypeScript", "TailwindCss"],
+    links: {
+      code: "https://github.com/Imad-t/skyWay",
+      demo: "https://sky-way.vercel.app/",
+    },
+    img: skyWay,
+  },
+  {
+    title: "Chat Page",
+    description:
+      "This is a simple chat page, suitable for any sort of website that supports account registration.",
+    tech: ["React", "SCSS"],
+    links: {
+      code: "https://github.com/Imad-t/chatPage",
+      demo: "https://chat-page-zeta.vercel.app/",
+    },
+    img: chatPage,
+  },
+];
+
+export default projects;
